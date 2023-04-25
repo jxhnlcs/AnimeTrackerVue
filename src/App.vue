231fa74fe323a3e12a5ed77eb1ed11b2d39e0cd0
John Lucas <joohnzay@gmail.com>
2023-04-25 10:58:08 -0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script setup>
 
+import { ref, computed, onMounted }from 'vue'
+
+const query = ref('')
+const my_anime = ref('')
+const search_results = ref([])
+
+const my_anime_asc = computed(() =>{
+  return my_anime.value.sort((a, b) =>{
+    return a.title.localCompare(b.title)
+  })
+})
 </script>
 
 <template>
-  
+  <main>Hello world</main>
 </template>
 
