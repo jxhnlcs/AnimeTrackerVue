10ced6e081df2eb97c191050a1170c6542134468
John Lucas <joohnzay@gmail.com>
2023-04-25 11:04:32 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,38 @@ const my_anime_asc = computed(() =>{
     return a.title.localCompare(b.title)
   })
 })
+
+const searchAnime = () => {
+  const url = `https://api.jikan.moe/v4/anime?q=${query.value}`
+  fetch(url)
+  .then(res => res.json())
+  .then(res => {
+    search_results.value = res.data
+  })
+}
+
+const handleInput = e => {
+  if (!e.target.value){
+    search_results.value = []
+  }
+}
+
+const addAnime = anime => {
+  search_results.value = []
+  query.value = ''
+
+  my_anime.value.push({
+    id: anime.mal_id,
+    title: anime.title,
+    image: anime.images.jpg.image_url,
+    total_episodes: anime.episodes,
+    watched_epidodes: 0
+  })
+
+  localStorage.setTime('my anime', JSON.stringify(my_anime.value))
+}
+
+
 </script>
 
 <template>
